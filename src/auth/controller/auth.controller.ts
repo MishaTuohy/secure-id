955449bf00d9822c15f35bc58509a32189e0736d
mtuohy <misha.tuohy@yahooinc.com>
2023-09-19 12:55:30 +0100

    extend auth controller

--- src/auth/controller/auth.controller.ts
+++ src/auth/controller/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Put, Delete } from '@nestjs/common';
 import { AuthService } from '../service/auth.service';
 
 @Controller('auth')
@@ -18,5 +18,35 @@ export class AuthController {
   @Post('/logout')
   async logoutUserByToken(@Body() tokenData: any): Promise<any> {
     return this.authService.logoutUserByToken(tokenData);
+  }
+
+  @Post('/password-reset/initiate')
+  async initiatePasswordReset(@Body() emailData: any): Promise<any> {
+    return this.authService.initiatePasswordReset(emailData);
+  }
+
+  @Put('/password-reset/complete')
+  async completePasswordReset(@Body() resetData: any): Promise<any> {
+    return this.authService.completePasswordReset(resetData);
+  }
+
+  @Put('/change-password')
+  async changePassword(@Body() changeData: any): Promise<any> {
+    return this.authService.changePassword(changeData);
+  }
+
+  @Put('/2fa/setup')
+  async setupTwoFactorAuth(@Body() setupData: any): Promise<any> {
+    return this.authService.setupTwoFactorAuth(setupData);
+  }
+
+  @Delete('/2fa/disable')
+  async disableTwoFactorAuth(@Body() userData: any): Promise<any> {
+    return this.authService.disableTwoFactorAuth(userData);
+  }
+
+  @Post('/verify-email')
+  async verifyEmail(@Body() verificationData: any): Promise<any> {
+    return this.authService.verifyEmail(verificationData);
   }
 }
--- src/auth/service/auth.service.ts
+++ src/auth/service/auth.service.ts
@@ -3,15 +3,39 @@ import { Injectable, NotImplementedException } from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
-  loginUserAndGetToken(loginData: any): any {
+  async loginUserAndGetToken(loginData: any): Promise<any> {
     throw new NotImplementedException();
   }
 
-  registerUser(registrationData: any): any {
+  async registerUser(registrationData: any): Promise<any> {
     throw new NotImplementedException();
   }
 
-  logoutUserByToken(tokenData: any): any {
+  async logoutUserByToken(tokenData: any): Promise<any> {
+    throw new NotImplementedException();
+  }
+
+  async initiatePasswordReset(emailData: any): Promise<any> {
+    throw new NotImplementedException();
+  }
+
+  async completePasswordReset(resetData: any): Promise<any> {
+    throw new NotImplementedException();
+  }
+
+  async changePassword(changeData: any): Promise<any> {
+    throw new NotImplementedException();
+  }
+
+  async setupTwoFactorAuth(setupData: any): Promise<any> {
+    throw new NotImplementedException();
+  }
+
+  async disableTwoFactorAuth(userData: any): Promise<any> {
+    throw new NotImplementedException();
+  }
+
+  async verifyEmail(verificationData: any): Promise<any> {
     throw new NotImplementedException();
   }
 }
